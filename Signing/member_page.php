947b483279c9c6521ae9ef50cc72f9d1c657803a
pius <101417933+pius2154@users.noreply.github.com>
2022-04-24 21:54:50 +0300

    Update member_page.php

--- Signing/member_page.php
+++ Signing/member_page.php
@@ -17,23 +17,34 @@ if(!isset($_SESSION['member_name'])){
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>member page</title>
+    <title>member</title>
 
     <!-- custom css file link -->
     <link rel="stylesheet" href="styles.css">
 
 </head>
 <body>
-    <div class="container">
-        <div class="content">
-            <h3>hi, <span>member</span></h3>
-            <h1>welcome <span><?php echo $_SESSION['member_name'] ?></span></h1>
-            <p>this is a member page</p>
-            <a href="Signin_form.php" class="btn">Signin</a>
-            <a href="Signup_form.php" class="btn">Signup</a>
-            <a href="Signout.php" class="btn">Signout</a>
+<section class="showcase">
+        <header>
+            <h2 class="logo">Keep Going</h2>
+        </header>
+       <!-- <video src="video.mp4" muted loop autoplay></video> -->
+        <div class="overlay"></div>
+        <div class="text">
+            <h2>Never Stop</h2>
+            <h3>Moving to your dreams</h3>
+            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit.Quis corporis cumque tempora officiis expedita aperiam quo sapiente ducimus ipsa repellendus?</p>
+            <a href="index.html">Move</a>
+            <a href="#"><?php echo $_SESSION['member_name'] ?></a>
+
         </div>
-    </div>
-    
+        <ul class="social">
+            <li><a href="#"><img src="instagram.png" alt=""></a></li>
+            <li><a href="#"><img src="instagram.png" alt=""></a></li>
+            <li><a href="#"><img src="instagram.png" alt=""></a></li>
+            <footer class="main-footer active-element"><span class="copyright">&copy;2022 A. Gabriel Hannadi Mary Justyn Dave</span></footer>
+        </ul>
+        
+    </section>
 </body>
 </html>
